b5589621b2043cecf079ebba815f89a6897aa904
idp <igordepaula1@gmail.com>
2018-05-11 17:18:37 -0300

    Made header ssr friendly

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,22 +11,28 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   COLAPSE_ON_SCROLL_TRESH : number = 20;
-  headerCollapsed : Boolean;
+  headerCollapsed : boolean;
+
+  isBrowser : boolean;
 
   logo : string = "https://vectr.com/idp/f3RoW0TgGR.svg?width=640&height=640&select=f3RoW0TgGRpage0";
 
-  constructor() { }
+  constructor(
+    @Inject(PLATFORM_ID) private  platformId : Object
+  ) { 
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   ngOnInit() {
-    window.addEventListener('scroll', this.scroll, true);
+    if(this.isBrowser) window.addEventListener('scroll', this.scroll, true);
   }
 
   ngOnDestroy() {
-      window.removeEventListener('scroll', this.scroll, true);
+    if(this.isBrowser) window.removeEventListener('scroll', this.scroll, true);
   }
 
   scroll : EventListener = (e) : void => {
-    this.headerCollapsed = window.scrollY >= this.COLAPSE_ON_SCROLL_TRESH;
+    if(this.isBrowser) this.headerCollapsed = window.scrollY >= this.COLAPSE_ON_SCROLL_TRESH;
   };
 
 }
