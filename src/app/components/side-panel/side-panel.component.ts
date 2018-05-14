09c4ab0ea8a7306735806c5eaace85c6cac6584e
idp <igordepaula1@gmail.com>
2018-05-14 15:35:17 -0300

    Finished side menu setup

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'side-panel',
@@ -7,15 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidePanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   open : boolean = false;
 
+  @Input() menuItems : MenuItemsConfig[] = [
+    {label: 'Home', action: ()=>{ this.router.navigateByUrl('/') }},
+    {label: 'About', action: ()=>{ this.router.navigateByUrl('/about') }},
+    {label: 'Special', subMenuExpanded:true, subMenu : [
+      {label: 'yo', action:()=>{alert('🦖 yo')} },
+      {label: 'test', action:()=>{alert('🎉 It works!!')} },  
+    ]}
+  ];
+
+  expandThing = true;
+
   ngOnInit() {
+
     setTimeout(()=>{
       this.open = true
     }, 1000)
 
   }
 
+
+
+
+}
+
+export interface MenuItemsConfig{
+  label : string,
+  icon? : string,
+  action? : Function,
+  subMenuExpanded? : boolean,
+  subMenu? : MenuItemsConfig[]
 }
