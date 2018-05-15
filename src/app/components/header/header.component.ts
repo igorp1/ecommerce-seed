6d46b522fadcec165a0b36f2849366bca45c605a
idp <igordepaula1@gmail.com>
2018-05-15 20:46:55 -0300

    fixed header + responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { PLATFORM_ID, APP_ID, Inject } from '@angular/core';
 import { ContextService } from '../../services/context.service';
+import { MenuItemsConfig } from '../side-panel/side-panel.component';
 
 @Component({
   selector: 'app-header',
@@ -17,8 +18,17 @@ export class HeaderComponent implements OnInit {
   isBrowser : boolean;
 
   // config
-  logo : string = "assets/whitelabel-logo.png";
-  siteTitle : string = "e-commerce seed" 
+  logo_small : string = "assets/whitelabel-logo.png";   // aspect ratio : square
+  logo_large : string = "assets/whitelabel-baner.png";  // aspect ratio : fb banner
+  siteTitle : string = "e-commerce seed";
+  
+  menuItems : MenuItemsConfig[] = [
+    {label:'ITEM 1'},
+    {label:'ITEM 2'},
+    {label:'ITEM 3'},
+    {label:'ITEM 4'},
+    {label:'ITEM 5'},
+  ]
 
   constructor(
     @Inject(PLATFORM_ID) private  platformId : Object,
